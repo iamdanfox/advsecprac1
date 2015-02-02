3417890a6653546d71fdb4e7200cf72b20e3eead
Dan Fox <iamdanfox@gmail.com>
2015-02-02 19:54:32 +0000

    client sequence numbers

--- client.js
+++ client.js
@@ -7,22 +7,35 @@ var DIFFIE_HELLMAN_PRIME = 'KbhkmZowCP8blHg4RYAP95kaIw=='; // length = 150
 
 
 
-
+var expectedSeqNumber = 0;
 
 var diffieHellman = crypto.createDiffieHellman(DIFFIE_HELLMAN_PRIME, 'base64');
 var diffieHellmanSharedSecret;
 
-var client = net.connect({ port: PORT }, function() {
+var socket = net.connect({ port: PORT }, function() {
   // do step 1
   diffieHellman.generateKeys()
-  client.write('1' + diffieHellman.getPublicKey('base64'));
+  socket.write('1' + diffieHellman.getPublicKey('base64'));
+  expectedSeqNumber = 2;
 });
 
-client.on('data', function(data) {
+socket.setEncoding('utf8');
+
+socket.on('data', function(data) {
+  console.assert(typeof data === 'string')
+
+  var sequenceNumber = parseInt(data[0], 10);
   var chunkBody = data.toString().slice(1).trim()
   console.assert(typeof chunkBody === 'string')
-  var serverPublicKey = new Buffer(chunkBody, 'base64');
 
-  diffieHellmanSharedSecret = diffieHellman.computeSecret(serverPublicKey, 'base64');
-  console.log('computed shared secret: '+diffieHellmanSharedSecret.toString('base64'));
+  if (expectedSeqNumber === sequenceNumber) {
+    // proceed
+    var serverPublicKey = new Buffer(chunkBody, 'base64');
+    diffieHellmanSharedSecret = diffieHellman.computeSecret(serverPublicKey, 'base64');
+    console.log('computed shared secret: '+diffieHellmanSharedSecret.toString('base64'));
+  } else {
+    console.error('[ABORTING] Invalid chunk. Was expecting sequenceNumber=' + (expectedSeqNumber + 1));
+    console.log('Received: "' + chunk + '"');
+    socket.end()
+  }
 });
