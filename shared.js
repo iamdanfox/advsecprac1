99b2b61a00800b9bb0252fb0505faf420b513a6b
Dan Fox <iamdanfox@gmail.com>
2015-02-02 21:02:56 +0000

    IV stuff

--- shared.js
+++ shared.js
@@ -1,10 +1,34 @@
 var crypto = require('crypto');
 
+var SYMMETRIC_KEY_LENGTH = 128;
+var RSA_IV_LENGTH = 128;
+var SYMMETRIC_CIPHER = 'aes-128-cbc';
+console.assert(crypto.getCiphers().indexOf(SYMMETRIC_CIPHER) != -1, 'Cipher must be present in openssl');
+
+// RSA_PKCS1_PADDING has no randomness
+// https://github.com/davedoesdev/ursa implements this
+
+
+
 module.exports = {
 
   PORT: 8125,
 
   DIFFIE_HELLMAN_PRIME: 'KbhkmZowCP8blHg4RYAP95kaIw==', // length = 150
+
+  // returns a binary buffer!
+  keyFromSharedSecret: function(sharedSecret) {
+    console.assert(typeof sharedSecret === 'string');
+    return crypto.pbkdf2Sync(sharedSecret, 'danisgreat', Math.pow(10,4), SYMMETRIC_KEY_LENGTH);
+  },
+
+  // returns a binary buffer!
+  ivFromSharedSecret: function(sharedSecret) {
+    console.assert(typeof sharedSecret === 'string');
+    iv = crypto.pbkdf2Sync(sharedSecret, 'danissupergreat', Math.pow(10,4), IV_LENGTH);
+    console.assert(Buffer.isBuffer(iv))
+    return iv;
+  },
 
   socketLoop: function(socket, responderFunction, expectedSeqNumber) {
 
@@ -40,5 +64,3 @@ module.exports = {
     });
   }
 }
-
-// crypto.pbkdf2Sync('hello, world', 'danisgreat', 20000, 128).toString('base64')
